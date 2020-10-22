f3594e75d303e498ece00814bc38fbf94ef0d06d
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-10-21 23:24:51 -0700

    Changed the output message found in main

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "Simple genetic algorithm" << endl; // prints Simple genetic algorithm
+    cout << "Start MMkCPP genetic algorithm" << endl; // prints Simple genetic algorithm
 
     GA ga = GA(argc, argv);
 
