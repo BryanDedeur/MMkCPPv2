7980f1f698f3bcbe5a6c57beebd87549a4b6d6ca
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-11-05 05:36:56 -0800

    Improved visualizations

--- visualizations/Graph.py
+++ visualizations/Graph.py
@@ -1,6 +1,13 @@
 # importing the required modules 
 import matplotlib.pyplot as plt 
 import numpy as np
+import random as random
+
+graphName = 'graph-gdb1'
+
+graphfile = '../benchmarks/mmkcpp/' + graphName + '.csv'
+infile = '../routes'
+outfile = '../img/route-visualization-' + graphName + '.png'
 
 adjacency_matrix = np.matrix([[1, 2], [3, 4]])
 
@@ -16,37 +23,88 @@ def load_graph_data(fname):
   adj = np.array(matrix)
   return adj
 
+def plotRoutes(fname, route):
+    f = open(fname, 'r')
+    for line in f:
+        arr = []
+        count = 0
+        start = ()
+        for token in line.split(' '):
+            if count != 0:
+                arr.append(int(token))
+            count += 1
+
+
+    f.close()
+    return 0
+
 def plotAdjacencyMatrix(adj):
     n = adj.shape[0]
     axis = np.linspace(0, 2*np.pi, n, endpoint=False)
     x, y = np.cos(axis), np.sin(axis)
-    # hide the axis
-    ax = plt.gca()
-    ax.axes.xaxis.set_ticks([])
-    ax.axes.yaxis.set_ticks([])
-    # plot the graph
-    plt.plot(x, y, 'bo', color='red', label='POINT')
-    sum = 0
-    for i in range(n):
-        plt.annotate(i,  # this is the text
-                     (x[i], y[i]),  # this is the point to label
-                     textcoords="offset points",  # how to position the text
-                     xytext=(0, 10),  # distance from text to points (x,y)
-                     ha='center',
-                     color='red')  # horizontal alignment can be left, right or center
-        for j in range(i + 1, n):
-            if adj[i, j] != -1:
-                sum += adj[i, j]
-                plt.plot((x[i], x[j]), (y[i], y[j]), color='blue')
-                plt.annotate(adj[i, j],  # this is the text
-                             ((x[i] + x[j]) / 2, (y[i] + y[j]) / 2),  # this is the point to label
+
+    f = open(infile, 'r')
+    robots = []
+    for line in f:
+        arr = []
+        count = 0
+        for token in line.split(' '):
+            if count != 0:
+                arr.append(int(token))
+            count += 1
+        robots.append(arr)
+    f.close()
+
+    fig, axs = plt.subplots(len(robots) + 1, figsize=(5, 5 * len(robots)))
+
+    count = 0
+    for ax in axs:
+        if count == len(robots):
+            ax.set_title(graphName)
+        else:
+            ax.set_title('R' + str(count) + " travel path")
+        # hide the axis
+        ax.axes.xaxis.set_ticks([])
+        ax.axes.yaxis.set_ticks([])
+        # make plots square
+        ax.set_aspect('equal', adjustable='box')
+        # plot the graph
+        ax.plot(x, y, 'bo', color='black', label='POINT', markersize=2)
+        for i in range(n):
+            ax.annotate(i, (x[i], y[i]),
+                         textcoords="offset points",
+                         xytext=(0, 5),
+                         ha='center',
+                         color='black')
+            for j in range(i + 1, n):
+                if adj[i, j] != -1:
+                    if count == len(robots):
+                        ax.plot((x[i], x[j]), (y[i], y[j]), color='blue', linewidth=1)
+                    else:
+                        ax.plot((x[i], x[j]), (y[i], y[j]), 'b--', color='grey', linewidth=.25)
+
+        count += 1
+
+
+    for r in range(len(robots)):
+        ax = axs[r]
+        color = (random.random(), random.random(), random.random())
+        count = 0
+        for k in range(len(robots[r])):
+            arr = robots[int(r)]
+            if count < len(robots[r]) - 1:
+                ax.plot((x[arr[k]], x[arr[k + 1]]), (y[arr[k]], y[arr[k + 1]]), c=color)
+                ax.annotate(adj[arr[k], arr[k + 1]],  # this is the text
+                             ((x[arr[k]] + x[arr[k + 1]]) / 2, (y[arr[k]] + y[arr[k + 1]]) / 2),  # this is the point to label
                              textcoords="offset points",  # how to position the text
                              xytext=(0, 5),  # distance from text to points (x,y)
                              ha='center',
-                             color='blue')  # horizontal alignment can be left, right or center
-    print("Sum of edges = ", sum)
+                             c=color)  # horizontal alignment can be left, right or center
+            count += 1
+
     plt.show()
+    fig.savefig(outfile)
     return
 
-adjacency_matrix = load_graph_data("../benchmarks/mmkcpp/graph-gdb19.csv")
+adjacency_matrix = load_graph_data(graphfile)
 plotAdjacencyMatrix(adjacency_matrix)
