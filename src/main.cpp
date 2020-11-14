58e080b1a57a4cfe865cb7725ffbd72874dd9a67
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-11-13 23:09:18 -0800

    Reduced repetition in main by creating bash

--- src/main.cpp
+++ src/main.cpp
@@ -6,67 +6,20 @@
  * @Date: 10/18/20
  */
 
-#include <time.h>
 #include <iostream>
-#include <fstream>
 #include "GA.h"
-#include <string>
 
 using namespace std;
 
-void runCommand(string commandStr) {
-    cout << " - " << commandStr << endl;
-    const char* command = commandStr.c_str();
-    int ret_val = system(command);
-
-    if(!(ret_val == 0 && errno == 0)) {
-        cerr << "There was an error with the command: " << ret_val << endl;
-    }
-}
-
 int main(int argc, char *argv[]) {
-    clock_t start, mid, end;
-    start = clock();
 
-    const int numRuns = 5;
-
-    // Start the GA
     GA ga = GA(argc, argv);
-    cout << "Starting MMkCPP genetic algorithm on graph " << ga.options.datafile << endl; // prints Simple genetic algorithm
-    double aveTime = 0;
-    for (int i = 1; i <= numRuns; i++) {
-        mid = clock();
-        ga.options.randomSeed = time(NULL);
+    cout << endl << "Starting GA on graph " << ga.options.graphName << " for " << ga.options.numRuns << " runs of " << ga.options.maxgens << " generations with population size " << ga.options.popSize << ": " << endl;
+    for (int i = 1; i <= ga.options.numRuns; i++) {
         ga.Init();
         ga.Run();
-        end = clock();
-        cout << "Run " << i << " seed(" << ga.options.randomSeed << "): "<< ga.options.maxgens << " generations of population size " << ga.options.popSize << " evolved in " << double(end - mid)/1000 << "s" << endl;
-        aveTime += double(end - mid);
     }
-    end = clock();
-    cout << "Finished " << numRuns << " runs in " << double(end - start) / 1000 << "s (" << (aveTime / numRuns) / 1000 << "s per runs)" << endl << endl;
-
-    // Write the best individual to a file
-    ga.best->WriteToFile(ga.options.decodedfile);
-
-    // Percentage best member is above lower bound
-    float percentAboveLowerBound = float(ga.best->totalTravelDistance)/float(ga.graph->sumEdges);
-
-    cout << "Best individual results:" << endl;
-    cout << " - Sum of edges vs travel distance: " << ga.graph->sumEdges << " vs "<< ga.best->totalTravelDistance << endl;
-    cout << " - Percentage above lower bound: " << (percentAboveLowerBound - 1) * 100 << "%" << endl << endl;
-
-    // Show where to find the output files
-    cout << "Fitness Results: " << ga.options.fitnessfile << endl;
-    cout << "Travel Results: " << ga.options.travelfile << endl;
-    cout << "Best Individual Decoded: " << ga.options.decodedfile << endl << endl;
-
-    // Call the visualization stuff
-    cout << "Running visualization commands: " << endl;
-    runCommand("python3 ../visualizations/TravelPlot.py " + ga.options.travelfile + " " + ga.options.datafile);
-    runCommand("python3 ../visualizations/FitnessPlot.py " + ga.options.fitnessfile + " " + ga.options.datafile);
-    runCommand("python3 ../visualizations/GraphTravelPlans.py " + ga.options.decodedfile + " " + ga.options.datafile);
-    runCommand("python3 ../visualizations/GraphTravelOverlap.py " + ga.options.decodedfile + " " + ga.options.datafile);
+    ga.Report();
 
     return 0;
 }
