de071f575e0888616879f66a972277e72ee14d40
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-11-13 23:08:43 -0800

    Increased const limits for CHC

--- src/Const.h
+++ src/Const.h
@@ -9,11 +9,11 @@
 #define CONST_H_
 
 #define MAXPOP 2000
-#define MAX_CHROMLENGTH 100
+#define MAX_CHROMLENGTH 200
 
 #define MAX_NUM_ROBOTS 10
 #define MAX_VERTICES 100
 #define NUM_VERTICES_PER_EDGE 2
-#define MAX_EDGES 100
+#define MAX_EDGES 200
 
 #endif /* CONST_H_ */
