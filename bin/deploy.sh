269c1d26e5f426b413b29afb58f80d2ac9fd16d3
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-11-25 13:14:50 -0800

    Improved the bash script and fixed previous issues with arguments

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,14 +5,14 @@ graph_directory=$(ls ../benchmarks/arc-routing/*.csv)
 ga_exe="./ga.exe"
 
 for graph in $graph_directory; do
-    "$ga_exe" "$graph" "30 2"
+    "$ga_exe" $graph 5 2 1000 1000
 done
 
 for graph in $graph_directory; do
-    "$ga_exe" "$graph" "30 5"
+    "$ga_exe" $graph 5 5 1000 1000
 done
 
 for graph in $graph_directory; do
-    "$ga_exe" "$graph" "30 1"
+    "$ga_exe" $graph 5 1 1000 1000
 done
 
