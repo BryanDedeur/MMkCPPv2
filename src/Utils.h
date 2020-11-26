2c1b9640862f4d4bbfdd1424e9d232fd797fce45
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-11-25 21:54:13 -0800

    Added file format helper

--- src/Utils.h
+++ src/Utils.h
@@ -19,6 +19,7 @@ using std::string;
 void ClearFile(std::string filename);
 void WriteBufToFile(std::string buf, std::string filename);
 std::string ExtractNameOfFile(std::string);
+std::string ExtractFileFormat(std::string);
 float RandomFraction();
 int Flip(float prob);
 int IntInRange(int low, int high);
